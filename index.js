d027d59d3f6eda3e615d70820b4f44a4f810d40f
Andrea <alops@localhost.localdomain>
2018-07-12 00:00:09 +0200

    add cose

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import AlbumList from './src/components/AlbumList';
 
 // creare un componenete
 const App = () => (
-    <View>
+    <View style={{ flex: 1 }}>
         <Header headerText={'Albums'} />
         <AlbumList />
     </View>
